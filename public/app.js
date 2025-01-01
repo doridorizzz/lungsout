619df6c0ce4543c31ae8d9623dd830ab18cb3ee0
doridorizzz <wkdnd999@gmail.com>
2025-01-01 21:30:44 +0900

    Add attendance app files

--- public/app.js
+++ public/app.js
@@ -7,11 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAzG-BIGsqncyy1OqNw3umz3MSteWyjmBg",
     authDomain: "lungsout-5afc5.firebaseapp.com",
     databaseURL: "https://lungsout-5afc5-default-rtdb.firebaseio.com",
-    projectId: "lungsout-5afc5",
-    storageBucket: "lungsout-5afc5.firebasestorage.app",
-    messagingSenderId: "949382654325",
-    appId: "1:949382654325:web:98aa2e267fd645935b8b3c",
-    measurementId: "G-7L7SBBXZ9V"
+    projectId: "lungsout-5afc5"
   };
 
 const app = initializeApp(firebaseConfig);
